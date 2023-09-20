34967ae2493a8a790ca8754484dd598c232144cd
Khanh Do <khanhdo.pmp@gmail.com>
2023-09-20 18:30:08 +0700

    Add exercise 01

---
+++ tests/exercises/01.spec.ts
@@ -0,0 +1,11 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('https://www.programsbuzz.com/user/login');
+  await page.getByLabel('Username').fill('myusername');
+  await page.getByLabel('Password').fill('mypassword');
+  await page.getByRole('button', { name: 'Log in' }).click();
+  await expect(page.locator('#edit-name--description')).toHaveText('Enter your ProgramsBuzz username.');
+  await expect(page.locator('#edit-pass--description')).toHaveText('Enter the password that accompanies your username.');
+  await expect(page.getByText('Error message Unrecognized username or password. Forgot your password?')).toBeVisible();
+});
